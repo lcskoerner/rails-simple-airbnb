59e7bf367ccdd7805daeab9578006173b14b5b13
Lucas Koerner <lcskoerner@gmail.com>
2020-08-12 23:26:28 -0400

    create / edit / delete

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,12 +1,19 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
+
   def index
     @flats = Flat.all
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+
+    redirect_to flat_path(@flat)
   end
 
   def new
+    @flat = Flat.new
   end
 
   def edit
@@ -16,8 +23,27 @@ class FlatsController < ApplicationController
   end
 
   def update
+    @flat.update(flat_params)
+
+    redirect_to flat_path(@flat)
   end
 
   def destroy
+    @flat.destroy
+
+    redirect_to flats_path
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address,
+                                 :description,
+                                 :number_of_guests,
+                                 :price_per_night)
   end
 end
